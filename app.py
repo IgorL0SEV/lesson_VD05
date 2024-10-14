174773a48f90d0153eb5fe90149e201a5138c453
IgorL0SEV <igor.v.losev@gmail.com>
2024-10-15 01:26:14 +0300

    write base code

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about_1')
+def about_1():
+    return render_template('about_1.html')
+
+@app.route('/about_2')
+def about_2():
+    return render_template('about_2.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
